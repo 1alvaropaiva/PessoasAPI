db1e5c7bb85e179967153f156d945aea7da600e2
Alvaro <alvaropaiva.dev@gmail.com>
2025-09-10 12:51:57 -0300

    add: nodemon

--- src/data/pessoasData.js
+++ src/data/pessoasData.js
@@ -1,6 +1,28 @@
 const { v4: uuidv4 } = require("uuid");
 
-// banco de dados em memória
-const pessoas = []; 
+// banco de dados em memória com dados mockados
+
+const pessoas = [
+  /*
+      {
+        id: 1,
+        matricula: uuidv4(), 
+        nome: "Alvaro",
+        idade: 25
+      },
+      {
+        id: 2,
+        matricula: uuidv4(),
+        nome: "Maria",
+        idade: 30
+      },
+      {
+        id: 3,
+        matricula: uuidv4(),
+        nome: "João",
+        idade: 40
+      }
+    */
+];
 
 module.exports = pessoas;
